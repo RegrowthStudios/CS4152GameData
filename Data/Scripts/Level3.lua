15762efa60dda0a658e924455d0784e1559653a7
ptsteadman <ptsteadman@gmail.com>
2015-04-08 21:56:50 -0400

    turrets oriented in level

--- Data/Scripts/Level3.lua
+++ Data/Scripts/Level3.lua
@@ -1,5 +1,20 @@
+turret1 = 0
+turret2 = 0
+turret3 = 0
+
 function onGameBuild()
-  eIDBase, eIDTurret = makeTurret(-6, -5, 3)
+  eIDTurret = makeTurret(7, 3.5, 8) 
+  turret1= eIDTurret
+  eIDTurret = makeTurret(4, 6, 1) 
+  turret2 = eIDTurret
+  eIDTurret = makeTurret(0, -8, 18) 
+  turret3 = eIDTurret
+  cID = ECS.getComponentID("BulletObject", turret1)
+  ECS.BulletObject.setOrientation(cID, 0, 0, 1.57) 
+  cID = ECS.getComponentID("BulletObject", turret2)
+  ECS.BulletObject.setOrientation(cID, 0, 3.14, 2.57) 
+  cID = ECS.getComponentID("BulletObject", turret3)
+  ECS.BulletObject.setOrientation(cID, 0, 0, 0) 
 end
 
 function onGameUpdate (dt)
@@ -7,22 +22,18 @@ function onGameUpdate (dt)
 	playerPosCID = ECS.getComponentID("Position", pEID)
 	x, y, z = ECS.Position.getPosition(playerPosCID)
   Client.setMessage(string.format("%i, %i, %i", x, y, z))
+
+
+
 end
 
 function makeTurret(x, y, z)
-  eIDBase = ECS.Templates.TurretBase()
-  cIDPhys = ECS.getComponentID("BulletObject", eIDBase)
-  ECS.BulletObject.createBody(cIDPhys)
-  cIDPos = ECS.getComponentID("Position", eIDBase)
-  ECS.BulletObject.setPosition(cIDPhys, x, y, z)
-  
   eIDTurret = ECS.Templates.Turret()
   cIDPhysTurret = ECS.getComponentID("BulletObject", eIDTurret)
   ECS.BulletObject.createBody(cIDPhysTurret)
+  ECS.BulletObject.setPosition(cIDPhysTurret, x, y, z)
   
-  ECS.BulletObject.Constraint.addFixed(cIDPhysTurret, "TurretSwivel", cIDPhys, 0, 0.7, 0)
-  
-  return eIDBase, eIDTurret
+  return eIDTurret
 end
 
 Vorb.register("onGameBuild", onGameBuild)
