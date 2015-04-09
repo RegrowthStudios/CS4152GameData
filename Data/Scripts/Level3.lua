199602062e797e8c284ad8b5edc5ebb776831443
ptsteadman <ptsteadman@gmail.com>
2015-04-08 22:51:19 -0400

    ugly turret level test

--- Data/Scripts/Level3.lua
+++ Data/Scripts/Level3.lua
@@ -1,6 +1,7 @@
 turret1 = 0
 turret2 = 0
 turret3 = 0
+t = 0
 
 function onGameBuild()
   eIDTurret = makeTurret(7, 3.5, 8) 
@@ -21,10 +22,14 @@ function onGameUpdate (dt)
 	pEID = State.getPlayer()
 	playerPosCID = ECS.getComponentID("Position", pEID)
 	x, y, z = ECS.Position.getPosition(playerPosCID)
+  t = t + dt
+  if t > 1 then
+    shootFromEntity(turret1)
+    shootFromEntity(turret2)
+    shootFromEntity(turret3)
+    t = 0    
+  end
   Client.setMessage(string.format("%i, %i, %i", x, y, z))
-
-
-
 end
 
 function makeTurret(x, y, z)
@@ -34,6 +39,23 @@ function makeTurret(x, y, z)
   ECS.BulletObject.setPosition(cIDPhysTurret, x, y, z)
   
   return eIDTurret
+end
+
+function shootFromEntity(eIDTurret)
+  -- Get player info
+  cID = ECS.getComponentID("Position", eIDTurret)
+  x,y,z = ECS.Position.getPosition(cID)
+  fx,fy,fz = ECS.Position.getForward(cID)
+  
+  -- Spawn ball
+  eID = ECS.Templates.Projectile()
+  cIDPos = ECS.getComponentID("Position", eID)
+  ECS.Position.setPosition(cIDPos, x, y, z) 
+
+  -- Shoot ball
+  cIDPhys = ECS.getComponentID("BulletObject", eID)
+  ECS.BulletObject.createBody(cIDPhys)
+  ECS.BulletObject.applyForce(cIDPhys, fx * 300, fy * 300, fz * 300)
 end
 
 Vorb.register("onGameBuild", onGameBuild)
