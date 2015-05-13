dc051655b18d605ce4676973d692eb643ca88a3a
ptsteadman <ptsteadman@gmail.com>
2015-05-13 19:14:30 -0400

    level 2 improvements

--- Data/Scripts/Level2.lua
+++ Data/Scripts/Level2.lua
@@ -8,7 +8,7 @@ flags["touched4"] = false
 flags["touched6"] = false
 flags["touched7"] = false
 flags["touched8"] = false
-flags["touched12"] = false
+flags["touched9"] = false
 currentRing = 0
 
 function onGameBuild()
@@ -17,14 +17,19 @@ function onGameBuild()
   -- Make end portal
   eID = ECS.Templates.Portal()
   cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 0.0, 8.0, -40.0)
-  ECS.Position.setQuaternion(cID, 0.005, -0.306, 0.014, 0.952)
+  ECS.Position.setPosition(cID, 0.0, 8.0, -43.0)
+  ECS.Position.setQuaternion(cID, 1.0, 0.0, 0.0, 0.0)
 
 
   eID = ECS.Templates.JumpPad()
   cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, -10.0, 5.8, -23)
-  ECS.Position.setQuaternion(cID, 0.6, 0.36, 0.59, -0.35)
+  ECS.Position.setPosition(cID, -9.2, -10.3, -22.0)
+  ECS.Position.setQuaternion(cID, -0.44, -0.127, -0.26, 0.96)
+
+  eID = ECS.Templates.JumpPad()
+  cID = ECS.getComponentID("Position", eID)
+  ECS.Position.setPosition(cID, -9.2, -10.3, -23.5)
+  ECS.Position.setQuaternion(cID, -0.44, -0.127, -0.26, 0.96)
 
 
   -- Make fixed ring
@@ -35,7 +40,7 @@ function onGameBuild()
   ECS.RingRotationFactor.set(rrfCID, 0.5)
 
   rrfCID = ECS.getComponentID("RingRotationFactor", 9)
-  ECS.RingRotationFactor.set(rrfCID, 3.0)
+  ECS.RingRotationFactor.set(rrfCID, 5.0)
   
   --loadMusic()
   -- Client.Sound.playMusicTrack("Electronic", 7.0)
@@ -67,28 +72,14 @@ end
 
 function onRingContact(id)
   currentRing = id
-  if id == 3 and not flags["touched3"] then
-    table.insert(messageQueue, createMessage("Narrative0", "I recently erased your memory,"))
-    table.insert(messageQueue, createMessage("Narrative1", "so you might feel a bit confused about your environment."))
-    flags["touched3"] = true 
-  end
-  if id == 5 and not flags["touched5"] then
-    table.insert(messageQueue, createMessage("Narrative2", "Oh, so you left that first ring?  Neat."))
-    table.insert(messageQueue, createMessage("Narrative3", "Spacebar to jump."))
-    flags["touched5"] = true
+  if id == 9 and not flags["touched9"] then
+    table.insert(messageQueue, createMessage("Narrative0", "If you roll over the YELLOW JUMPPAD,"))
+    table.insert(messageQueue, createMessage("Narrative1", "you'll get a break from solving puzzles."))
+    flags["touched9"] = true 
   end
   if id == 6 and not flags["touched6"] then
-    table.insert(messageQueue, createMessage("Narrative4", "You're on the Array,"))
-    table.insert(messageQueue, createMessage("Narrative5", "the last record of humanity in the universe."))
+    table.insert(messageQueue, createMessage("Narrative2", "You can push around GREY RINGS with GREEN RINGS."))
     flags["touched6"] = true
-  end
-  if id == 7 and not flags["touched7"] then
-    table.insert(messageQueue, createMessage("Narrative6", "You can rotate green rings like this one with Q and E."))
-    flags["touched7"] = true
-  end
-  if id == 11 and not flags["touched11"] then
-    table.insert(messageQueue, createMessage("Narrative7", "Don't go near that glowing portal thing..."))
-    flags["touched11"] = true
   end
 end
 
