27c81ed94dfb530e3627a2a80baa42093b0e1e4e
ptsteadman <ptsteadman@gmail.com>
2015-05-13 16:54:42 -0400

    fix blade and laser turrets a bit

--- Data/Scripts/LaserTurret.lua
+++ Data/Scripts/LaserTurret.lua
@@ -12,10 +12,10 @@ Vorb.register("Update.LaserTurret", function (eID, dt)
   if ex < 1.52 and ex > -1.52 then 
     -- todo?  check if abs(ex-0), abs(ey-3.14) etc is too big?
   -- snap towards forwards
-    ECS.Position.setOrientation(cIDPos, 0, 0.0, ez)
+    ECS.Position.setOrientation(cIDPos, 0.0, 0.0, ez)
   else 
   -- snap towards backwards
-    ECS.Position.setOrientation(cIDPos, 0, -3.14, ez)
+    ECS.Position.setOrientation(cIDPos, 0.0, math.pi, ez)
   end 
 
   local px, py, pz = ECS.Position.getPosition(cIDPos)
