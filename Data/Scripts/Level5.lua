fc4e53a18e59ff0abe2940177012ed8e89de5718
ptsteadman <ptsteadman@gmail.com>
2015-05-13 22:35:45 -0400

    starting level 5

--- Data/Scripts/Level5.lua
+++ Data/Scripts/Level5.lua
@@ -22,92 +22,53 @@ FIRST_RED = 9
 
 
 function onGameBuild()
-  Client.Renderer.setMode("Low")
+  --Client.Renderer.setMode("Low")
 
   -- tutorial turrets on first ring, firing into blades
 
 
-  eID = ECS.Templates.LaserTurret()
+  eID = ECS.Templates.Turret()
   cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 5.2, -5.2, 6.75)
-  ECS.Position.setQuaternion(cID, -0.37, 0.926, 0.010, 0.024)
+  ECS.Position.setPosition(cID, 0.0, 7.5, 8)
+  ECS.Position.setQuaternion(cID, -0.6, 0.0, 0.737, 0.0)
 
-  eID = ECS.Templates.LaserTurret()
+  eID = ECS.Templates.Turret()
   cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 6.5, 0.0, 12.5)
-  ECS.Position.setQuaternion(cID, -0.005, 0.005, 0.694, 0.719)
+  ECS.Position.setPosition(cID, 0.0, 7.5, 10)
+  ECS.Position.setQuaternion(cID, -0.6, 0.0, 0.737, 0.0)
+
+  eID = ECS.Templates.Turret()
+  cID = ECS.getComponentID("Position", eID)
+  ECS.Position.setPosition(cID, 0.0, 7.5, 12)
+  ECS.Position.setQuaternion(cID, -0.6, 0.0, 0.737, 0.0)
+
+
+
+--[[
+
+  turrets[3] = {}
+  turrets[3]["eid"] = 8
+  turrets[3]["type"] = "projectile"
+  turrets[3]["timer"] = 0
+  turrets[3]["shootRate"] = 4
+  turrets[3]["force"] = 250
+
+ 
+  turrets[4] = {}
+  turrets[4]["eid"] = 9
+  turrets[4]["type"] = "projectile"
+  turrets[4]["timer"] = 1.5
+  turrets[4]["shootRate"] = 4
+  turrets[4]["force"] = 250
 
   
-  eID = ECS.Templates.LaserTurret()
-  cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, -7.4, -1.01, -24.1)
-  ECS.Position.setQuaternion(cID, 0.65, 0.75, -0.012, 0.013)
-
-  eID = ECS.Templates.LaserTurret()
-  cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 1.0, -7.4, -24.1)
-  ECS.Position.setQuaternion(cID, 0.011, 1.0, 0.0, -0.01)
-
-  rrfCID = ECS.getComponentID("RingRotationFactor", FIRST_GREEN)
-  ECS.RingRotationFactor.set(rrfCID, 2.75)
-
-  -- add the lasers to the fixed black ring
-  bCID = ECS.getComponentID("BulletObject", FIRST_BLACK)
-  ECS.BulletObject.setMass(bCID, 0)
-
-  eID = ECS.Templates.LaserTurret()
-  cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 5.9, 4.5, -24.0)
-  ECS.Position.setQuaternion(cID, 0.043, -0.015, 0.920, 0.388)
-
-
-  eID = ECS.Templates.LaserTurret()
-  cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 7.42, 3.4, -29.5)
-  ECS.Position.setQuaternion(cID, 0.78, -0.624, 0.011, 0.009)
-
-  eID = ECS.Templates.LargeBlade()
-  cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 8.0, 3.4, -22.9)
-  ECS.Position.setQuaternion(cID, 0.016, -0.015, 0.717, 0.697)
-
-
-
-  -- sweep this laser on this ring over the other lasers
-  
-  EID_ROTATING_TURRET_1  = ECS.Templates.LaserTurret()
-  cID = ECS.getComponentID("Position", EID_ROTATING_TURRET_1)
-  ECS.Position.setPosition(cID, 7.9, 0.95, -42.0)
-  ECS.Position.setQuaternion(cID, 0.748, -0.663, 0.002, 0.001)
-
-
-
-
-  rrfCID = ECS.getComponentID("RingRotationFactor", SECOND_GREEN)
-  ECS.RingRotationFactor.set(rrfCID, 3.2)
-
-  -- turrets on the red ring
-  
-  EID_ROTATING_TURRET_2  = ECS.Templates.LaserTurret()
-  cID = ECS.getComponentID("Position", EID_ROTATING_TURRET_2)
-  ECS.Position.setPosition(cID, 7.45, 0.89, -75.5)
-  ECS.Position.setQuaternion(cID, 0.752, -0.659, -0.003, -0.003)
-
-  EID_ROTATING_TURRET_3  = ECS.Templates.LaserTurret()
-  cID = ECS.getComponentID("Position", EID_ROTATING_TURRET_3)
-  ECS.Position.setPosition(cID, -7.28, -0.89, -75.5)
-  ECS.Position.setQuaternion(cID, 0.653, 0.757, -0.012, 0.013)
-
-
-
-
-  -- end portal!  yay!  this level was hard to make!
-
-  eID  = ECS.Templates.Portal()
-  cID = ECS.getComponentID("Position", eID)
-  ECS.Position.setPosition(cID, 1.98, -7.44, -87.5)
-  ECS.Position.setQuaternion(cID, -0.12, 0.99, -0.003, -0.02)
-
+  turrets[5] = {}
+  turrets[5]["eid"] = 10
+  turrets[5]["type"] = "projectile"
+  turrets[5]["timer"] = 2.5
+  turrets[5]["shootRate"] = 4
+  turrets[5]["force"] = 250
+]]
   --loadMusic()
   -- Client.Sound.playMusicTrack("Electronic", 7.0)
 end
